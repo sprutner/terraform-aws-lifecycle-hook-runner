33a7e6f35e94540a217272177fb0a7c0561b0db5
srutner <srutner@credomobile.com>
2017-09-01 13:42:21 -0700

    fixed policy

--- main.tf
+++ main.tf
@@ -72,6 +72,7 @@ resource "aws_iam_policy" "lifecycle" {
                 "ssm:DescribeDocument*",
                 "ssm:GetDocument",
                 "ssm:DescribeInstance*",
+                "ec2:CreateNetworkInterface"
             ]
         }
     ]
